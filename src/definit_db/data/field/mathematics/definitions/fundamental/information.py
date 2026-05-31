705e8d8c3d3391867919a164e9f5dc2c8203b376
K4liber <janbielecki94@gmail.com>
2026-05-31 15:38:17 +0200

    information

--- src/definit_db/data/field/mathematics/definitions/fundamental/information.py
+++ src/definit_db/data/field/mathematics/definitions/fundamental/information.py
@@ -2,15 +2,21 @@ from definit.definition.definition import Definition
 from definit.definition.definition_key import DefinitionKey
 
 from definit_db.data.field import FieldName
+from definit_db.data.field.mathematics.definitions.fundamental.object import OBJECT
 
 
 class _Information(Definition):
     def _get_content(self) -> str:
-        return """
+        return f"""
 An abstract concept that refers to something which has the power to inform. 
 At the most fundamental level, it pertains to the interpretation (perhaps formally) of that which may be 
 sensed, or their abstractions. Any natural process that is not completely random and any observable 
 pattern in any medium can be said to convey some amount of information.
+
+---
+
+Whether an {OBJECT.key.get_reference("object")} is present or absent at a given place
+conveys information about that place.
 """
 
 
--- src/definit_db/data/field/mathematics/definitions/fundamental/object.py
+++ src/definit_db/data/field/mathematics/definitions/fundamental/object.py
@@ -8,10 +8,9 @@ class _Object(Definition):
     def _get_content(self) -> str:
         return """
 An object is any entity that can be considered, named, or manipulated within a mathematical context.
-"""
 
-    def _get_example(self) -> str:
-        return """
+---
+
 "3" can be named, compared, and used in a statement. We can call it an object.
 """
 
